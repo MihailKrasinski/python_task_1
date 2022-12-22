e0508e50146b0b13a46b894146c2b0cd7fe209fd
MihailKrasinski <mihailkrasinski@mail.ru>
2022-12-22 12:49:24 +0300

    Add child QueriesDB with self methods to parent InitDB

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ class InitDB:
         self.user = user
         self.password = password
 
-    def __engine(self):
+    def engine(self):
         return create_engine("postgresql+psycopg2://{user}:{pw}@{host}/{db}"
                                     .format(host=self.host, db=self.database, user=self.user,
                                             pw=self.password))
 
-    def __init_connector(self):
+    def init_connector(self):
         connection = connect(
             host=self.host,
             user=self.user,
@@ -27,25 +27,27 @@ class InitDB:
         connection.autocommit = True
         return connection
 
-    def create_schema(self, schem):
-        cursor = self.__init_connector().cursor()
-        cursor.execute(schem)
-        self.disconnect()
-
     def disconnect(self):
-        self.__init_connector().cursor().close()
-        self.__init_connector().close()
+        self.init_connector().cursor().close()
+        self.init_connector().close()
+
+
+class QueriesDB(InitDB):
+    def create_schema(self, schem):
+        cursor = super().init_connector().cursor()
+        cursor.execute(schem)
+        super().disconnect()
 
     def req_query_out(self, query):
-        with self.__init_connector().cursor() as cursor:
+        with super().init_connector().cursor() as cursor:
             cursor.execute(query)
             res = cursor.fetchall()
-            self.disconnect()
+            super().disconnect()
             return res
 
     def json_to_sql(self, table, dir_json_file):
         df = pd.read_json(dir_json_file)
-        df.to_sql(table, self.__engine(), index=False, if_exists='append')
+        df.to_sql(table, super().engine(), index=False, if_exists='append')
 
     def out_to_xml(self, query):
         df = pd.DataFrame(self.req_query_out(query), columns=[f'column_{i + 1}' for i in range(len(self.req_query_out(query)[0]))])
@@ -57,19 +59,21 @@ class InitDB:
 
 
 if __name__ == "__main__":
-    init = InitDB(creds['HOST'], creds['DBNAME'], creds['USER'], creds['PASSWORD'])
-    init.create_schema(schema)
+    # init = InitDB(creds['HOST'], creds['DBNAME'], creds['USER'], creds['PASSWORD'])
+    queries = QueriesDB(creds['HOST'], creds['DBNAME'], creds['USER'], creds['PASSWORD'])
+    queries.create_schema(schema)
 
     # filling tables
-    init.json_to_sql('rooms', '/home/mikra/MikraPythonProjects/python_task_1/data/in/rooms.json')
-    init.json_to_sql('students', '/home/mikra/MikraPythonProjects/python_task_1/data/in/students.json')
+    queries.json_to_sql('rooms', '/home/mikra/MikraPythonProjects/python_task_1/data/in/rooms.json')
+    queries.json_to_sql('students', '/home/mikra/MikraPythonProjects/python_task_1/data/in/students.json')
 
     # print result of SQL request query from DB
-    print(init.req_query_out(query_1))
-    print(init.req_query_out(query_2))
-    print(init.req_query_out(query_3))
-    print(init.req_query_out(query_4))
+    print(queries.req_query_out(query_1))
+    print(queries.req_query_out(query_2))
+    print(queries.req_query_out(query_3))
+    print(queries.req_query_out(query_4))
 
     # puts data of SQL query you ask to xml / JSON from DB
-    init.out_to_xml(query_3)
-    init.out_to_json(query_3)
+    queries.out_to_xml(query_3)
+    queries.out_to_json(query_3)
+
